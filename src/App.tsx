470f7851ceb45f9468e0ded30a10e8e14d521fd3
Nyak <nyak@Nyaks-Air.mshome.net>
2021-05-04 14:31:28 +0700

    hatsu commito

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react';
+import * as key from './eNum';
 
-function App() {
+interface State {
+  inputData?: string,
+  lastData?: string,
+  operator?: string,
+}
+
+const initialState: State = {
+  inputData: '',
+  lastData: '',
+  operator: '',
+};
+
+const calReducer = (state: State, action: { type: string; payload: State; }) => {
+  switch (action.type) {
+    case 'OPERATE':
+      return {
+        inputData: action.payload.inputData,
+        lastData: action.payload.lastData,
+        operator: action.payload.operator,
+      }
+    default:
+      return state;
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(calReducer, initialState);
+  console.log(state);
+  const { inputData, lastData, operator } = state;
+  const addToInput = (key: string | number) => dispatch({ type: 'OPERATE', payload: { ...state, inputData: inputData + key }});
+  const addZero = (key: number) => {
+    if (inputData === '') return;
+    addToInput(key);
+  };
+  const addPoint = (key: string) => {
+    if ((/\./).test(inputData)) return;
+    addToInput(key);
+  };
+  const allClear = () => dispatch({ type: 'OPERATE', payload: { inputData: '', lastData: '' }});
+  const handleClear = () => dispatch({ type: 'OPERATE', payload: { inputData: '' }});
+  const handleOperate = (key: string) => dispatch({ type: 'OPERATE', payload: { lastData: inputData, inputData: '', operator: key }});
+  const handleDelete = () => dispatch({ type: 'OPERATE', payload: { inputData: inputData.substring(0, inputData.length - 1)}});
+  const handleCalculate = () => {
+    let result: number;
+    switch (operator) {
+      case '+':
+        result = +(lastData) + +(inputData);
+        break;
+      case '-':
+        result = +(lastData) - +(inputData);
+        break;
+      case '*':
+        result = +(lastData) * +(inputData);
+        break;
+      case '/':
+        result = +(lastData) / +(inputData);
+        break;
+      default:
+        return;
+    };
+    allClear();
+    dispatch({ type: 'OPERATE', payload: { inputData: result.toString() }});
+  };
+  const changePolarity = () => {
+    const data = +(inputData);
+    const changed = -data;
+    dispatch({ type: 'OPERATE', payload: { inputData: changed.toString() }});
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <input className="input" type="text" value={inputData} />
+      <div className="pads">
+        <button onClick={allClear} className="blue" >{key.AC}</button>
+        <button onClick={handleClear} className="blue" >{key.C}</button>
+        <button onClick={handleDelete} className="blue" >{key.Del}</button>
+        <button onClick={() => handleOperate(key.plus)} className="red" >{key.plus}</button>
+        <button onClick={() => addToInput(key.num7)}>{key.num7}</button>
+        <button onClick={() => addToInput(key.num8)}>{key.num8}</button>
+        <button onClick={() => addToInput(key.num9)}>{key.num9}</button>
+        <button onClick={() => handleOperate(key.minus)} className="red" >{key.minus}</button>
+        <button onClick={() => addToInput(key.num4)}>{key.num4}</button>
+        <button onClick={() => addToInput(key.num5)}>{key.num5}</button>
+        <button onClick={() => addToInput(key.num6)}>{key.num6}</button>
+        <button onClick={() => handleOperate(key.multiply)} className="red" >{key.multiply}</button>
+        <button onClick={() => addToInput(key.num1)}>{key.num1}</button>
+        <button onClick={() => addToInput(key.num2)}>{key.num2}</button>
+        <button onClick={() => addToInput(key.num3)}>{key.num3}</button>
+        <button onClick={() => handleOperate(key.divide)} className="red" >{key.divide}</button>
+        <button onClick={() => addPoint(key.point)}>{key.point}</button>
+        <button onClick={() => addZero(key.num0)}>{key.num0}</button>
+        <button onClick={changePolarity}>{key.negative}</button>
+        <button onClick={handleCalculate} className="red" >{key.equal}</button>
+      </div>
     </div>
   );
 }
---
+++ src/eNum.tsx
@@ -0,0 +1,22 @@
+export const AC = 'AC';
+export const C = 'C';
+export const Del = 'DEL';
+export const plus = '+';
+export const minus = '-';
+export const multiply = '*';
+export const divide = '/';
+export const equal = '=';
+
+export const point = '.';
+export const negative = '-';
+
+export const num1 = 1;
+export const num2 = 2;
+export const num3 = 3;
+export const num4 = 4;
+export const num5 = 5;
+export const num6 = 6;
+export const num7 = 7;
+export const num8 = 8;
+export const num9 = 9;
+export const num0 = 0;
